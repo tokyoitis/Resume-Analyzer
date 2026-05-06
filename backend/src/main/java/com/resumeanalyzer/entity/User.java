cc2414c010d5a5a1942169bf86321d36e7e92b18
tokyoitis <anushkayadav20602@gmail.com>
2026-05-06 23:34:05 +0530

    Started Project

---
+++ backend/src/main/java/com/resumeanalyzer/entity/User.java
@@ -0,0 +1,50 @@
+package com.resumeanalyzer.entity;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.management.relation.Role;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Resumes> resumes;
+
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Jobs>jobs;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt=LocalDateTime.now();
+    }
+}
